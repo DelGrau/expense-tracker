309dca207a38ff3dc94cbc6fca2f044757f84cfb
DelGrau <alanhipermassa@gmail.com>
2026-03-31 21:49:52 -0300

    Testes da classe Amount

--- src/test/java/AmountTest.java
+++ src/test/java/AmountTest.java
@@ -1,24 +1,31 @@
-package com.delgrau.expensetracker.tests;
-
 import com.delgrau.expensetracker.model.Amount;
-import junit.framework.TestCase;
+import org.junit.jupiter.api.Test;
 
-public class AmountTest extends TestCase {
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AmountTest {
+    @Test
     public void testWhenCreatingWithInteger () {
         var value = Amount.fromCurrency(10);
+
         assertEquals(10.00, value.getAmount());
-        assertEquals(1000, value.getCents());
+        assertEquals(1000L, value.getCents());
     }
 
+    @Test
     public void testWhenCreatingWithFloat () {
         var value =Amount.fromCurrency(10.50f);
+
         assertEquals(10.50, value.getAmount());
-        assertEquals(1050, value.getCents());
+        assertEquals(1050L, value.getCents());
     }
 
+    @Test
     public void testWhenCreatingWithLong () {
         var value = Amount.fromCents(1005);
+
         assertEquals(10.05, value.getAmount());
         assertEquals(1005L, value.getCents());
     }
 }
+
