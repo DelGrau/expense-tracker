e55a6e6560fc3e5860c1e9cbad259a3d6515e996
DelGrau <alanhipermassa@gmail.com>
2026-04-01 00:41:37 -0300

    CLI backbone created successfully.

--- src/main/java/com/delgrau/expensetracker/ExpenseTracker.java
+++ src/main/java/com/delgrau/expensetracker/ExpenseTracker.java
@@ -1,17 +1,63 @@
 package com.delgrau.expensetracker;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class ExpenseTraker {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import com.delgrau.expensetracker.model.Amount;
+import picocli.CommandLine;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Command;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+import java.util.concurrent.Callable;
+
+@Command(name = "expense-tracker", /* mixinStandardHelpOptions = true,*/ version = "1.0",
+    description = "Personal Expense Tracker via command line.")
+public class ExpenseTracker implements Callable<Integer> {
+
+    @Option(names = {"-h", "--help"}, usageHelp = true, description = "Exibe esta mensagem de ajuda")
+    private boolean helpRequested;
+
+    @Option(names = {"-V", "--version"}, versionHelp = true, description = "Exibe a versão do programa")
+    private boolean versionRequested;
+
+    @Option(names = {"-d", "--description"}, required = true)
+    private String description;
+
+    @Option(names = {"-v", "--value"}, required = true)
+    private Amount value;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public double getValue() {
+        return value.getAmount();
+    }
+
+    public long getCents() {
+        return value.getCents();
+    }
+
+    @Override
+    public Integer call() throws Exception {
+        try {
+            System.out.println("Processando...");
+            System.out.println("Descricao: " + getDescription());
+            System.out.println("Valor: " + getValue() +
+                    " (ou " + getCents() + " centavos).");
+            return 0;
+        } catch (Exception e) {
+            System.out.println("Oops, algo deu errado...");
         }
+        return 500;
+    }
+
+    public static void main(String[] args) {
+        CommandLine cmd = new CommandLine(new ExpenseTracker());
+
+        cmd.registerConverter(Amount.class, s -> {
+            double val = Double.parseDouble(s.replace(",", "."));
+            return Amount.fromCurrency(val);
+        });
+
+        var exitCode = cmd.execute(args);
+        System.exit(exitCode);
     }
 }
