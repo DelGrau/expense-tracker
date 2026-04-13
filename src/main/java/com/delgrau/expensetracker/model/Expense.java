ef807c06cb7f79ad0a825e7f0d5d88b0d3f3d010
DelGrau <alanhipermassa@gmail.com>
2026-04-13 00:38:17 -0300

    few chages to save our expenses, added toString() method

--- src/main/java/com/delgrau/expensetracker/model/Expense.java
+++ src/main/java/com/delgrau/expensetracker/model/Expense.java
@@ -1,18 +1,26 @@
 package com.delgrau.expensetracker.model;
 
 public class Expense {
-    private long id;
+    private long id = 0;
     private String description;
     private Amount value;
 
     public Expense (String d, Double v) {
+        id++;
+        this.description = d;
+        this.value = Amount.fromCurrency(v);
+    }
+
+    public Expense (long i, String d, Double v) {
+        this.id = i;
         this.description = d;
         this.value = Amount.fromCurrency(v);
     }
 
     public long getId() {
-        return 0L;
+        return id;
     }
+
     public String getDescription() {
         return description;
     }
@@ -23,5 +31,14 @@ public class Expense {
 
     public long getCents() {
         return value.getCents();
+    }
+
+    public String toString() {
+        /*
+         * # ID  Date       Description  Amount
+         * # 1   2026-04-13  Test         R$20
+         */
+
+        return String.format("# ID  Description  Amount\n# %-3d  %-11s  R$%s", getId(), getDescription(), value.toString());
     }
 }
