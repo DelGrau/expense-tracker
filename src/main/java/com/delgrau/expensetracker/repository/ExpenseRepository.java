c51f13939e8cd179a601197eb8c30450b0007a9b
DelGrau <alanhipermassa@gmail.com>
2026-04-21 20:36:50 -0300

    Parsing `catgegory` as a string

--- src/main/java/com/delgrau/expensetracker/repository/ExpenseRepository.java
+++ src/main/java/com/delgrau/expensetracker/repository/ExpenseRepository.java
@@ -1,6 +1,7 @@
 package com.delgrau.expensetracker.repository;
 
 import com.delgrau.expensetracker.model.Amount;
+import com.delgrau.expensetracker.model.Category;
 import com.google.gson.*;
 import com.google.gson.reflect.TypeToken;
 import com.delgrau.expensetracker.model.Expense;
@@ -21,6 +22,12 @@ public class ExpenseRepository {
             .registerTypeAdapter(Amount.class, (JsonDeserializer<Amount>) (json, typeOfT, context) -> {
                 int cents = json.getAsInt();
                 return Amount.fromCurrency(cents / 100.0);
+            })
+            .registerTypeAdapter(Category.class, (JsonSerializer<Category>) (src, typpeOfSrc, context) -> {
+                return new JsonPrimitive(src.getDescription());
+            })
+            .registerTypeAdapter(Category.class, (JsonDeserializer<Category>) (json, typeOfT, context) -> {
+                return Category.fromString(json.getAsString());
             })
             .setPrettyPrinting()
             .create();
